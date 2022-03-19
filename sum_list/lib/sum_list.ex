01b6809cba598c82a7b4edf625afcfc5614b3f71
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-03-19 11:32:16 -0300

    first commit

---
+++ sum_list/lib/sum_list.ex
@@ -0,0 +1,18 @@
+defmodule SumList do
+  @moduledoc """
+  Documentation for `SumList`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SumList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
