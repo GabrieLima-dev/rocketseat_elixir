f1dea13aab1614a71e5d3ea78a306f96e177101a
Gabriel Lima <gabrieldesouzalima@gmail.com>
2022-03-22 20:58:51 -0300

    test initial

--- sum_list/test/sum_list_test.exs
+++ sum_list/test/sum_list_test.exs
@@ -1,8 +1,16 @@
 defmodule SumListTest do
+  # sempre utilizar essa linha.
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  # função pública que sempre será testada
+  describe "call/1" do
+    test "return the list sum" do
+      list = [1, 2, 3]
+      response = SumList.call(list)
+
+      expect_response == "banana"
+
+      assert response == expect_response
+    end
   end
 end
